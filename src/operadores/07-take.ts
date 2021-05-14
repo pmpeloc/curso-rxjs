aa5509728541afa5ac435c7bd60289a0cf2a5817
Pablo Misael Peloc <pablo.peloc@blimop.tech>
2021-05-14 17:41:07 -0300

    take

---
+++ src/operadores/07-take.ts
@@ -0,0 +1,14 @@
+import { of } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+
+const numeros$ = of(1,2,3,4,5).pipe(
+    tap(console.log)
+)
+
+numeros$.pipe(
+    tap(console.log),
+    take(3)
+).subscribe({
+    next: val => console.log('next: ', val),
+    complete: () => console.log('complete')
+});
