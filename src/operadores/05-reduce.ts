a73a2fe2db6ccb313e9200181db5ce4be26bda21
Pablo Misael Peloc <pablo.peloc@blimop.tech>
2021-05-15 15:37:50 -0300

    ejercicios finales

--- src/operadores/05-reduce.ts
+++ src/operadores/05-reduce.ts
@@ -5,7 +5,7 @@ const numbers = [1,2,3,4,5];
 
 const totalReducer = (acumulador: number, valorActual: number) => {
     return acumulador + valorActual;
-} ;
+};
 
 const total = numbers.reduce(totalReducer, 0);
 console.log('total arr', total);
