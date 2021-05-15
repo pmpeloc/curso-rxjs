f69394497259840ccbbc088a38b065ddc9eae37e
Pablo Misael Peloc <pablo.peloc@blimop.tech>
2021-05-15 11:03:46 -0300

    mergeMap

---
+++ src/transformacion/02-mergeMap.ts
@@ -0,0 +1,24 @@
+import { fromEvent, interval, of } from 'rxjs';
+import { map, mergeMap, take, takeUntil } from 'rxjs/operators';
+
+const letras$ = of('a', 'b', 'c');
+
+letras$.pipe(
+    mergeMap((letra) => interval(1000).pipe(
+        map(i => letra + i),
+        take(3)
+    ))
+)//.subscribe({
+//     next: val => console.log('next: ', val),
+//     complete: () => console.log('Complete')
+// });
+
+const mousedown$ = fromEvent(document, 'mousedown');
+const mouseup$ = fromEvent(document, 'mouseup');
+const interval$ = interval();
+
+mousedown$.pipe(
+    mergeMap(() => interval$.pipe(
+        takeUntil(mouseup$)
+    ))
+).subscribe(console.log);
