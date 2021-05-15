c08228c434e6cde73388f30d5365dee40fc5a7ee
Pablo Misael Peloc <pablo.peloc@blimop.tech>
2021-05-15 13:01:38 -0300

    concat

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,10 @@
+import { concat, interval } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+const interval$ = interval(1000);
+
+concat(
+    interval$.pipe(take(3)),
+    interval$.pipe(take(2)),
+    [1,2,3,4]
+).subscribe(console.log);
